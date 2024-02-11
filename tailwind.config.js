f71083b9b8490d1e0e61f1c570aa3b37f80c3b8d
Drew Myers <acmyers@us.ibm.com>
2024-02-11 14:20:13 -0500

    add favicon, commit tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'forest': '#2d353b',
+        'cream': '#d3c6aa',
+        'sprout': '#a7c080',
+        'salmon': '#e67e80',
+        'sky': '#7fbbb3',
+      },
+    },
+  },
+  plugins: [],
+}
